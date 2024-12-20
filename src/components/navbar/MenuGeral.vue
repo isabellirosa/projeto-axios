6c29ac7a9d0c61f0476d57f33267b4af23a3d91a
fujulia <juliafu713@gmail.com>
2024-12-19 23:31:30 -0300

    FEAT: Home feita

---
+++ src/components/navbar/MenuGeral.vue
@@ -0,0 +1,51 @@
+<script setup>
+
+</script>
+
+<template>
+    <header>
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/filmes">Filmes</router-link>
+      <router-link to="/tv">Programas de TV</router-link>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+
+header {
+  position: fixed; /* Deixa o menu fixo no topo */
+  top: 0;
+  left: 0;
+  width: 100%;
+  height:15vh;
+  background-color: transparent;
+  color: white;
+  font-size: 25px;
+  padding-right: 50px;
+  display: flex;
+  align-items: center;
+  justify-content:flex-end;
+  z-index: 10; /* Garante que o menu fique sobre o conteúdo */
+ font-weight: 500;
+ 
+  transition: background-color 0.3s ease; /* Transição suave para o fundo */
+}
+
+/* Navegação dentro do menu */
+nav {
+  display: flex;
+  column-gap:60px;
+}
+
+nav a {
+  text-decoration: none;
+  color: white;
+  transition: color 0.3s;
+}
+
+nav a:hover {
+  font-weight: 600;
+}
+</style>
